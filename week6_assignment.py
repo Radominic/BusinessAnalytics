e2c70286eff18465b2b33076e0a12cbf4be8dea5
Radominic <wjdrmf314@gmail.com>
2019-10-18 23:37:45 +0900

    practice6 update
    add EDA

--- week6_assignment.py
+++ week6_assignment.py
@@ -10,14 +10,19 @@ from sklearn.linear_model import LogisticRegression
 import numpy as np
 from sklearn.metrics import recall_score, f1_score, precision_score
 from sklearn.model_selection import StratifiedKFold
+from sklearn.feature_selection import chi2, f_classif,mutual_info_classif
 
 #%% For assignment 
 # US Presidential elections data load 
 elec=pd.read_csv('https://drive.google.com/uc?export=download&id=1fq9qDqXLiUm0un_saxAUpPsSJa05F_bV', index_col=0)
 county=pd.read_csv('https://drive.google.com/uc?export=download&id=1LciKFXkb3MmpXFEHDk1Db8YFsK0liF3a') 
 
+county.shape
+
 data=elec.merge(county, left_on='FIPS', right_on='fips', how='left')
+
 data['target']=(data['votes_dem_2016']>data['votes_gop_2016'])*1 
+
 data = elec[elec['county_name']!='Alaska'].merge(county, left_on='FIPS', right_on='fips', how='left')
 data_ak=elec[elec['county_name']=='Alaska'].drop_duplicates (['votes_dem_2016', 'votes_gop_2016']) 
 data_ak['FIPS']=2000
@@ -25,7 +30,34 @@ data_ak=data_ak.merge(county, left_on='FIPS', right_on='fips', how='left')
 data=pd.concat((data, data_ak), axis=0).sort_values ('fips') 
 data['target']=(data['votes_dem_2016']>data['votes_gop_2016'])*1
 
-var=[x for x in data.columns if 'RHI' in x or 'EDU' in x or 'AGE' in x or 'SEX' in x]
+vartype = ['AFN','AGE','BPS','BZA','EDU','HSD','HSG','INC','LFE','LND','MAN','NES','POP','PST','PVY','RHI','RTN','SBO','SEX','VET','WTN']
+selvars = [x for x in data.columns if x[:3] in vartype]
+len(selvars)
+
+#correlation
+corr = data[['target']+selvars].corr()
+corr_target = corr[['target']]
+corr_target=corr_target.abs()
+corr_target=corr_target.sort_values('target',ascending=False)
+
+corr = corr[['target']]
+corr=corr.drop('target')
+corr['TYPE']=[x[:3] for x in corr.index]
+corr['ABS_CORR']=corr['target'].abs()
+
+mean_corr = corr.groupby(['TYPE'])['ABS_CORR'].mean()
+mean_corr = mean_corr.sort_values(ascending=False)
+
+Fscore = f_classif(data[selvars],data['target'])
+
+Fscore = pd.DataFrame({'F':Fscore[0],'pvalue':Fscore[1]},index = selvars)
+Fscore = Fscore.sort_values('F',ascending=False)
+
+MI = mutual_info_classif(data[selvars],data['target'])
+MI = pd.DataFrame({'MI':MI},index=selvars)
+MI = MI.sort_values('MI',ascending = False)
+
+var=[x for x in data.columns if 'RHI' in x or 'HSG' in x or 'AGE' in x or 'EDU' in x]
 X=data[var]
 y=data['target']
 
